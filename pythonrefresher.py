1ee120529ea8b15558faf9db0b7633434ba0b68c
fredweedon <129555419+fredweedon@users.noreply.github.com>
2023-03-31 15:23:16 -0600

    Add files via upload

---
+++ pythonrefresher.py
@@ -0,0 +1,9 @@
+year = 2001
+
+
+
+if 2000 <= year < 2100: 
+    print("Welcome to the 21st century!")
+    
+else:
+    print("You are before or after the 21st century")
---
+++ pythonrefresherfunctionslesson8.py
@@ -0,0 +1,11 @@
+age = 12 
+
+name = 'Fred'
+
+todayIsCold = False
+
+def hello(name="Benita", age=2):
+    print("Hello {} you are {} years old".format(name, age))
+     
+
+hello("Doris", 7)
